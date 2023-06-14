42e71bcd5ab20ab0112d131c74fc44b98f4d6b99
Wilasta Kurniawan <44308357+vendenberg@users.noreply.github.com>
2023-06-14 20:05:08 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/pertemuan11/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pertemuan11
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool _loading = false;
+  List<dynamic> _users = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const FittedBox(
+          child: Text(
+            "API | Wilasta Kurniawan 201011401307",
+          ),
+        ),
+      ),
+      body: _users.isNotEmpty
+          ? ListView.builder(
+              itemCount: _users.length,
+              itemBuilder: ((context, index) {
+                return Card(
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 15.0,
+                    vertical: 10.0,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        Image.network(_users[index]['image']),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(_users[index]['firstName']),
+                            Text(_users[index]['email']),
+                            Text(_users[index]['phone']),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              }),
+            )
+          : Center(
+              child: _loading
+                  ? const CircularProgressIndicator()
+                  : ElevatedButton(
+                      onPressed: loadUserList,
+                      child: const Text("fetch users from dummyjson.com"),
+                    ),
+            ),
+    );
+  }
+
+  loadUserList() async {
+    setState(() {
+      _loading = true;
+    });
+    var res = await http.get(Uri.https("dummyjson.com", "users"));
+    if (res.statusCode == 200) {
+      var jsonData = jsonDecode(res.body);
+      if (jsonData['users'].isNotEmpty) {
+        setState(() {
+          _users = jsonData['users'];
+          _loading = false;
+        });
+      }
+    }
+  }
+}
